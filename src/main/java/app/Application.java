62452e0e437c290df95a936b6e7c84a94890b2a3
uliana1237 <ugrigina@gmail.com>
2022-12-19 14:13:52 +0300

    events consumer works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,4 +1,12 @@
 package app;
 
-public class Application {
+import io.github.humbleui.jwm.Event;
+
+import java.util.function.Consumer;
+
+public class Application implements Consumer<Event> {
+    @Override
+    public void accept(Event event) {
+
+    }
 }
